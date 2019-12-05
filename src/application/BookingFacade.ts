2e7f5dfafac443a03c6912ae6adfafc3c1327ace
Anass BEN YAZID <benyazidanass@gmail.com>
2019-12-05 05:08:19 +0100

    implement onion architecture & define domain models

---
+++ src/application/BookingFacade.ts
@@ -0,0 +1,12 @@
+export class BookingFacade{
+
+    public async BookTaxi(): Promise<boolean>{
+        return false;
+    }
+
+    public async cancelReservation() : Promise<boolean> {
+        return false;
+    }
+
+
+}
